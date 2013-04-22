9e0ec0faebabc1bea6c2ae2e590182690db3324b
rc678 <rosheenmail@gmail.com>
2013-04-22 12:24:58 -0300

    Update Friends.java
    This is the skeleton code.

--- Friends.java
+++ Friends.java
@@ -1,17 +1,37 @@
 public class Friends{
   
+  Vertex[] adjLL; 
   
   public Friends(){
+    
+      //initiallized the adjLL with the int from the 1st line read by the scanner
+      adjLL = new Vertex[sc.nextInt()]; 
 
-
-  }
-
-    //This method builds the adjacency Linked List
-    public static void build(){
-
-
+      //This method builds the adjacency Linked List
+      public static void build(Vertex friend){
+  
+  
+      }
+      
+      //This method will get the subgraph from the built adjLL
+      public static Vertex[] subgraph(){
+        
+      }
+      
+      //This method gets the shortest Path from two friends and returns the head of the linked list
+      public static Vertex shortestPath(){
+        
+      }
+      
+      //Gets the cliques from the original graph
+      public static Vertex[] cliques(){
+        
+      }
+      
+      //This method checks to see if a person is a "connecot" and then it returns the name of that person.
+      public static String connectors(){
+        
+      }
+      
     }
-
-
-
-}
+  }
