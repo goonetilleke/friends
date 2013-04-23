7a14c4f6fa628958b11401357d8a42e5919933fc
rc678 <rc678@eden.rutgers.edu>
2013-04-23 16:58:48 -0300

    Update driver.java
    hiiii

--- apps/driver.java
+++ apps/driver.java
@@ -8,15 +8,44 @@ public class driver{
       Friends friend = new friend();
       
       BufferedReader br = new BufferReader(new InputStreamReader(System.in));
-      System.out.print("Enter deck file name");
+      System.out.print("Enter text file name");
       
       Scanner sc = new Scanner(new File(br.readLine()));
+      
+      while ((choice = getChoice()) != 5) {
+    		switch(getChoice()) {
+			case 1: subgraph(); break;
+			case 2: shortestPath(); break;
+			case 3: cliques(); break;
+			case 4: connectors(); break;
+			}
+		}
     
       /*This method will call the make vertex method for each line
       *The make vertex method should turn the string into a node and then input the node into the adjLL
       */
-      while(sc.hasNext(){
-         friend.makeVertex(sc.next());
+      
+      int size = sc.nextInt(); 
+      for(int i = 1; i<size; i++){
+          friend.makeVertex(sc);
+          System.out.println("sending scanner for loop");
       }
+      
+      public static int getChoice(){
+      throws IOException {
+          System.out.println();
+          System.out.println("1. subgraph");
+          System.out.println("2. shortest path");
+          System.out.println("3. cliques");
+          System.out.println("4. connectors");
+          System.out.println("5. to quit");
+          System.out.print("\tEnter Choice => ");
+    	  Integer n = Integer.parseInt(br.readLine());
+          while (n < 1 || n > 5) {
+    		System.out.print("\tYour choice must be between 1 and 5, reenter => ");
+		  }
+		    System.out.println();
+		    return n;
+      }   
   }
 }
