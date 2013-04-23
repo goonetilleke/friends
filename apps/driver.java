f7eaed92d0b5b38e98adda58b5a52a35e2f0682a
rc678 <rc678@eden.rutgers.edu>
2013-04-23 17:48:37 -0300

    Update driver.java

--- apps/driver.java
+++ apps/driver.java
@@ -1,38 +1,46 @@
-import java.io.BufferedReader;
+package Friend;
 
-public class driver{
-    
-    public staatic void main(String[] args)
-    throws IOExcepetion{
+import java.io.*;
+import java.util.Scanner;
+
+public class driver {
+	
+	static BufferedReader br; 
+
+	public static  void main(String[] args)
+    throws IOException{
       
-      Friends friend = new friend();
+      Friends friend = new Friends();
       
-      BufferedReader br = new BufferReader(new InputStreamReader(System.in));
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       System.out.print("Enter text file name");
       
       Scanner sc = new Scanner(new File(br.readLine()));
-      
-      while ((choice = getChoice()) != 5) {
-    		switch(getChoice()) {
-			case 1: subgraph(); break;
-			case 2: shortestPath(); break;
-			case 3: cliques(); break;
-			case 4: connectors(); break;
-			}
-		}
     
       /*This method will call the make vertex method for each line
       *The make vertex method should turn the string into a node and then input the node into the adjLL
       */
-      
       int size = sc.nextInt(); 
       for(int i = 1; i<size; i++){
           friend.makeVertex(sc);
           System.out.println("sending scanner for loop");
       }
       
-      public static int getChoice(){
-      throws IOException {
+      br = new BufferedReader(new InputStreamReader(System.in));
+      int choice;
+  	  while ((choice = getChoice()) != 5) {
+  		  switch(getChoice()) {
+  		  case 1: friend.subgraph(); break;
+  		  case 2: friend.shortestPath(); break;
+  		  //check cliques later!!!!!!!!!!!!!!!!!
+  		  case 3: friend.cliques(null); break;
+  		  case 4: friend.connectors(); break;
+  		  }
+  	  }
+	}
+  	
+      
+      public static int getChoice() throws NumberFormatException, IOException{
           System.out.println();
           System.out.println("1. subgraph");
           System.out.println("2. shortest path");
@@ -40,12 +48,12 @@ public class driver{
           System.out.println("4. connectors");
           System.out.println("5. to quit");
           System.out.print("\tEnter Choice => ");
-    	  Integer n = Integer.parseInt(br.readLine());
+    	  int n = Integer.parseInt(br.readLine());
           while (n < 1 || n > 5) {
     		System.out.print("\tYour choice must be between 1 and 5, reenter => ");
 		  }
 		    System.out.println();
 		    return n;
-      }   
-  }
-}
+      }
+	}
+    
