a43fe009d89c914489bd5953266b2eb2a7fdb674
rc678 <rc678@eden.rutgers.edu>
2013-04-23 17:49:05 -0300

    Update Friends.java

--- apps/Friends.java
+++ apps/Friends.java
@@ -1,47 +1,54 @@
+package Friend;
+ 
+import java.io.*;
+import java.util.Scanner;
+
 public class Friends{
-  
+  //Scanner sc;
   Vertex[] adjLL; 
   
   public Friends(){
-    
+    //this.sc=sc;
       //initiallized the adjLL with the int from the 1st line read by the scanner
-      adjLL = new Vertex[sc.nextInt()]; 
+      adjLL = new Vertex[15]; 
 
-
+  }
       //This method will take the input from the scanner and put it into a node.
       //After creating a node, we should call the build method to build the adjLL.
       //So every time we make a new node we will insert it into the adjLL
       //w.c big O = O(n) i think...
-      public static void makeVertex(Scanner sc){
-        
-        
+      public void makeVertex(Scanner sc){
+
       }
 
       //This method builds the adjacency Linked List
-      public static void build(Vertex friend){
+      public void build(Vertex friend){
   
   
       }
       
       //This method will get the subgraph from the built adjLL
-      public static Vertex[] subgraph(){
+      public  Vertex[] subgraph(){
+		return adjLL;
         
       }
       
       //This method gets the shortest Path from two friends and returns the head of the linked list
-      public static Vertex shortestPath(){
+      public Vertex shortestPath(){
+		return null;
         
       }
       
       //Gets the cliques from the original graph
-      public static Vertex[] cliques(String school){
+      public Vertex[] cliques(String school){
+		return adjLL;
         
       }
       
       //This method checks to see if a person is a "connecot" and then it returns the name of that person.
-      public static String connectors(){
+      public String connectors(){
+		return null;
         
       }
-      
-    }
-  }
+      }
+	
