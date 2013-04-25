e8c7e9f6e18853be7e4c09f76d27716f6cabe462
rc678 <rc678@eden.rutgers.edu>
2013-04-25 14:20:54 -0300

    Update Vertex.java

--- structures/Vertex.java
+++ structures/Vertex.java
@@ -1,20 +1,24 @@
 package Friend;
-public class Vertex{
-  
-  String name;
-  boolean inSchool;
-  String schoolName;
-  boolean visited;
-  int dfsNum;
-  Vertex next;
-  
-  public Vertex(String name, boolean inSchool, String schoolName, Vertex next){
-    this.name = name; 
-    this.inSchool = inSchool;
-    this.schoolName = schoolName;
-    this.next = next;
-    this.dfsNum = -1;
-    this.visited = false; 
 
-  }
+public class Vertex {
+
+  String name;
+	boolean inSchool;
+	String schoolName;
+	boolean visited;
+	int dfsNum;
+	Neighbor neighbors;
+	//int index; 
+
+	public Vertex(String name, boolean inSchool, String schoolName, Neighbor nbr) {
+		this.name = name;
+		this.inSchool = inSchool;
+		this.schoolName = schoolName;
+		//this.next = next;
+		this.dfsNum = -1;
+		this.visited = false;
+		//this.index = -1; 
+		this.neighbors = nbr; 
+		
+	}
 }
