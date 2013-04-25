19e31883733073c3d5453aa6fff900ad6cbe8972
rc678 <rc678@eden.rutgers.edu>
2013-04-25 13:33:34 -0300

    Update driver.java

--- apps/driver.java
+++ apps/driver.java
@@ -4,59 +4,70 @@ import java.io.*;
 import java.util.Scanner;
 
 public class driver {
-	
-	static BufferedReader br; 
 
-	public static  void main(String[] args)
-    	throws IOException{
-      
-      
-	      
-	      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	      System.out.print("Enter text file name");
-	      
-	      Scanner sc = new Scanner(new File(br.readLine()));
-	    
-	      /*This method will call the make vertex method for each line
-	      *The make vertex method should turn the string into a node and then input the node into the adjLL
-	      */
-	      int size = sc.nextInt(); 
-	      //I put friends after so we can take the size given and put it as a paremeter 
-	      Friends friend = new Friends(size);
-	      for(int i = 1; i<size; i++){
-	      	//take next line from scanner as parameter so its a string and not giving the scanner hope its ok
-	          friend.makeVertex(sc.nextLine());
-	          System.out.println("sending scanner for loop");
-	      }
-	      
-	      br = new BufferedReader(new InputStreamReader(System.in));
-	      int choice;
-	  	  while ((choice = getChoice()) != 5) {
-	  		  switch(getChoice()) {
-	  		  case 1: friend.subgraph(); break;
-	  		  case 2: friend.shortestPath(); break;
-	  		  //check cliques later!!!!!!!!!!!!!!!!!
-	  		  case 3: friend.cliques(null); break;
-	  		  case 4: friend.connectors(); break;
-	  		  }
-	  	  }
+	static BufferedReader br;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter text file name");
+
+		Scanner sc = new Scanner(new File(br.readLine()));
+
+		/*
+		 * This method will call the make vertex method for each lineThe make
+		 * vertex method should turn the string into a node and then input the
+		 * node into the adjLL
+		 */
+		int size = sc.nextInt();
+		// I put friends after so we can take the size given and put it as a
+		// paremeter
+		Friends friend = new Friends(size);
+
+		// Changed the for loop to a while loop. This will loop through the
+		// whole text file
+		while (sc.hasNextLine()) {
+			int counter = 1;
+			friend.build(sc.nextLine(), counter);
+			counter++;
+			System.out.println("sending scanner for loop");
 		}
-	  	
-      
-      public static int getChoice() throws NumberFormatException, IOException{
-          System.out.println();
-          System.out.println("1. subgraph");
-          System.out.println("2. shortest path");
-          System.out.println("3. cliques");
-          System.out.println("4. connectors");
-          System.out.println("5. to quit");
-          System.out.print("\tEnter Choice => ");
-    	  int n = Integer.parseInt(br.readLine());
-          while (n < 1 || n > 5) {
-    		System.out.print("\tYour choice must be between 1 and 5, reenter => ");
-		  }
-		    System.out.println();
-		    return n;
-      }
+
+		br = new BufferedReader(new InputStreamReader(System.in));
+		int choice;
+		while ((choice = getChoice()) != 5) {
+			switch (getChoice()) {
+			case 1:
+				friend.subgraph();
+				break;
+			case 2:
+				friend.shortestPath();
+				break;
+			// check cliques later!!!!!!!!!!!!!!!!!
+			case 3:
+				friend.cliques(null);
+				break;
+			case 4:
+				friend.connectors();
+				break;
+			}
+		}
 	}
-    
+
+	public static int getChoice() throws NumberFormatException, IOException {
+		System.out.println();
+		System.out.println("1. subgraph");
+		System.out.println("2. shortest path");
+		System.out.println("3. cliques");
+		System.out.println("4. connectors");
+		System.out.println("5. to quit");
+		System.out.print("\tEnter Choice => ");
+		int n = Integer.parseInt(br.readLine());
+		while (n < 1 || n > 5) {
+			System.out
+					.print("\tYour choice must be between 1 and 5, reenter => ");
+		}
+		System.out.println();
+		return n;
+	}
+}
