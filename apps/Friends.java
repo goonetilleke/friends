979a6efbd34820c7deb7181a8255728df5217f9e
rc678 <rc678@eden.rutgers.edu>
2013-04-25 13:32:59 -0300

    Update Friends.java

--- apps/Friends.java
+++ apps/Friends.java
@@ -1,81 +1,85 @@
 package Friend;
- 
-import java.io.*;
-import java.util.Scanner;
 
-public class Friends{
-  //Scanner sc;
-  Vertex[] adjLL; 
-  
-  HashMap<String, Vertex> ppl; 
-  
-  public Friends(int size){
-    //this.sc=sc;
-      //initiallized the adjLL with the int from the 1st line read by the scanner
-      adjLL = new Vertex[size]; 
-      //initializes a new hashmap to keep track of people
-      ppl = new HashMap<String, Vertex>(1000, 2.0f);
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
-  }
-      //This method will take the input from the scanner and put it into a node.
-      //After creating a node, we should call the build method to build the adjLL.
-      //So every time we make a new node we will insert it into the adjLL
-      //w.c big O = O(n) i think...
-      public void makeVertex(String s){
-    	 String input=s;
-    	 String splitVal="|";
-    	 String[] subSplits =  input.split(splitVal);
-    	 String name= subSplits[1];
-    	 boolean inSchool;
-    	 if (subSplits[2].equals("y")){
-    		 inSchool=true;
-    	 } else{
-    		 inSchool=false;
-    	 }
-    	 String school=null;
-    	 if (inSchool==true){
-    		school= subSplits[3];
-    	 }
-    	  Vertex v=new Vertex(name, inSchool,school);
-    	  //calls build to fill the adjLL with the vertexs. 
-    	  //We have to figure out what to do with the relationships though.
-    	  //Maybe have a hastable that has the person's name and then a vertex object of all their
-    	  //information like what school they go to, etc.
-    	  build(v);
- 
-      }
+public class Friends {
+	// Scanner sc;
+	Vertex[] adjLL;
 
-      //This method builds the adjacency Linked List
-      public void build(Vertex friend){
-  
-  	//gets vertex and fills adjLL with the name of each person
-  	for(int i = 0; i<adjLL.length; i++){
-  		adjLL[i] = friend;
-  	}
-      }
-      
-      //This method will get the subgraph from the built adjLL
-      public  Vertex[] subgraph(){
+	HashMap<String, Vertex> ppl;
+	HashMap<String, Integer> index;
+	int numVertex;
+
+	public Friends(int size) {
+		// this.sc=sc;
+		// initiallized the adjLL with the int from the 1st line read by the
+		// scanner
+		numVertex = size;
+		adjLL = new Vertex[size];
+		// initializes a new hashmap to keep track of people
+		ppl = new HashMap<String, Vertex>(1000, 2.0f);
+		index = new HashMap<String, Integer>(1000, 2.0f);
+
+	}
+
+	// This method builds the adjacency Linked List
+	public void build(String s, int i) {
+		String input = s;
+		String splitVal = "|";
+		String[] subSplits = input.split(splitVal);
+		if (i <= numVertex) { // adds vertex into adjLL until i == number from
+								// 1st line
+			String name = subSplits[1].toLowerCase();
+			boolean inSchool;
+
+			if (subSplits[2].equals("y")) {
+				inSchool = true;
+			} else {
+				inSchool = false;
+			}
+
+			String school = null;
+			if (inSchool == true) {
+				school = subSplits[3];
+			}
+
+			Vertex v = new Vertex(name, inSchool, school, null);
+			ppl.put(name, v); // puts vertex into hashtable, key is person name
+			index.put(name, i); // puts index of person in adjLL, key is person
+								// name
+			adjLL[i] = v; // puts the node into the adjLL
+		}
+
+		String name1 = subSplits[1];
+		String name2 = subSplits[2];
+
+	}
+
+	// This method will get the subgraph from the built adjLL
+	public Vertex[] subgraph() {
 		return adjLL;
-        
-      }
-      
-      //This method gets the shortest Path from two friends and returns the head of the linked list
-      public Vertex shortestPath(){
+
+	}
+
+	// This method gets the shortest Path from two friends and returns the head
+	// of the linked list
+	public Vertex shortestPath() {
 		return null;
-        
-      }
-      
-      //Gets the cliques from the original graph
-      public Vertex[] cliques(String school){
+
+	}
+
+	// Gets the cliques from the original graph
+	public Vertex[] cliques(String school) {
 		return adjLL;
-        
-      }
-      
-      //This method checks to see if a person is a "connecot" and then it returns the name of that person.
-      public String connectors(){
+
+	}
+
+	// This method checks to see if a person is a "connecot" and then it returns
+	// the name of that person.
+	public String connectors() {
 		return null;
-        
-      }
-      }
-	
+
+	}
+}
