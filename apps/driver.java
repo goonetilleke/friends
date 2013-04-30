c45845a829c6476a3c9b1a01d934f11dab120802
rc678 <rc678@eden.rutgers.edu>
2013-04-30 13:35:58 -0300

    Update driver.java

--- apps/driver.java
+++ apps/driver.java
@@ -1,84 +1,216 @@
 package Friend;
 
-import java.io.*;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
-public class driver {
+public class Friends {
+	// Scanner sc;
+	Vertex[] adjLL;
+	// might get rid of ppl
+	HashMap<String, Vertex> ppl;
+	HashMap<String, Integer> index;
+	HashMap<String, ArrayList<String>> students;
+	int numVertex;
 
-	static BufferedReader br;
-	private static Scanner sc;
+	public Friends(int size) {
+		// this.sc=sc;
+		// initiallized the adjLL with the int from the 1st line read by the
+		// scanner
+		numVertex = size;
+		adjLL = new Vertex[size];
+		// initializes a new hashmap to keep track of people
+		ppl = new HashMap<String, Vertex>(1000, 2.0f);
+		index = new HashMap<String, Integer>(1000, 2.0f);
 
-	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter text file name");
-
-		sc = new Scanner(new File(br.readLine()));
-
-		/*
-		 * This method will call the make vertex method for each lineThe make
-		 * vertex method should turn the string into a node and then input the
-		 * node into the adjLL
-		 */
-		Friends friend = null;
-		boolean flag = false;
-		int counter = 0;
-		while (sc.hasNextLine()) {
-			if (flag == false) {
-				flag = true;
-				String size = sc.nextLine();
-				int val = Integer.parseInt(size);
-				friend = new Friends(val);
-			}
-
-			String s = sc.nextLine();
-			System.out.println(s);
-			friend.build(s, counter);
-			counter++;
-		}
-		br = new BufferedReader(new InputStreamReader(System.in));
-		int choice;
-		while ((choice = getChoice()) != 5) {
-			switch (getChoice()) {
-			case 1:// subgraph
-				System.out.println("Enter school");
-				Scanner sc1 = new Scanner(System.in);
-				String input = sc1.nextLine();
-				friend.subgraph(input);
-				break;
-			case 2:// shortest path
-				System.out.println("Enter start name");
-				Scanner sc2 = new Scanner(System.in);
-				String input1 = sc2.nextLine();
-				System.out.println("Enter ending name");
-				String input2 = sc2.nextLine();
-				friend.shortestPath(input1, input2);
-				break;
-			// check cliques later!!!!!!!!!!!!!!!!!
-			case 3: // cliques
-				friend.cliques(null, null);
-				break;
-			case 4:// connectors
-				friend.connectors();
-				break;
-			}
-		}
 	}
 
-	public static int getChoice() throws NumberFormatException, IOException {
-		System.out.println();
-		System.out.println("1. subgraph");
-		System.out.println("2. shortest path");
-		System.out.println("3. cliques");
-		System.out.println("4. connectors");
-		System.out.println("5. to quit");
-		System.out.print("\tEnter Choice => ");
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt(); 
-		while (n < 1 || n > 5) {
-			System.out.print("\tYour choice must be between 1 and 5, reenter => ");
+	// This method builds the adjacency Linked List
+	public void build(String s, int i) {
+		String input = s;
+		String[] subSplits = input.split("\\|");
+		students = new HashMap<String, ArrayList<String>>();
+		ArrayList<String> temp = new ArrayList<String>();
+
+		if (i < numVertex) { // adds vertex into adjLL until i == number from
+								// 1st line
+			String name = subSplits[0].toLowerCase();
+			boolean inSchool;
+			if (subSplits[1].equals("y")) {
+				inSchool = true;
+			} else {
+				inSchool = false;
+			}
+
+			String school = null;
+			if (inSchool == true) {
+				school = subSplits[2];
+			}
+			if (inSchool == true) {
+				temp.add(name);
+				students.put(school, temp);
+
+			}
+			Vertex v = new Vertex(name, inSchool, school, null);
+			ppl.put(name, v); // puts vertex into hashtable, key is person name
+			index.put(name, i); // puts index of person in adjLL, key is person
+								// name
+			adjLL[i] = v; // puts the node into the adjLL
+			return;
 		}
-		System.out.println();
-		return n;
+
+		// adding the neighbors to the adjLL
+		String name1 = subSplits[0];
+		String name2 = subSplits[1];
+
+		int v1 = index.get(name1); // name1
+		int v2 = index.get(name2); // name2
+
+		adjLL[v1].neighbors = new Neighbor(name2, adjLL[v1].neighbors);
+		adjLL[v2].neighbors = new Neighbor(name1, adjLL[v2].neighbors);
+
+	}
+
+	// This method will get the subgraph from the built adjLL
+	public Vertex[] subgraph(String s) {
+		Vertex[] subgraph = new Vertex[students.get(s).size()];
+		if (students.containsKey(s)) {
+			// make temp array of arraylist in students hashtable
+			ArrayList<String> temp = students.get(s);
+			for (int i = 0; i <= temp.size(); i++) {
+				String key = temp.get(i);
+				// int ind = index.get(key);
+				// name, inschool, schoolname, nbr
+				Vertex v = new Vertex(key, true, s, null);
+				subgraph[i] = v;
+			}
+		} else {
+			return null;
+		}
+
+		for (int j = 0; j < subgraph.length; j++) {
+			String name = subgraph[j].name;
+			int curr = index.get(name);
+			Neighbor ptr = adjLL[curr].neighbors;
+			while (ptr != null) {
+				String person = ptr.name;
+				if (ppl.get(person).schoolName.equalsIgnoreCase(s)) {
+					if (subgraph[j].neighbors == null) {
+						subgraph[j].neighbors = ptr;
+					} else {
+						subgraph[j].neighbors.next = ptr;
+					}
+				}
+				ptr = ptr.next;
+			}
+
+		}
+		return adjLL;
+
+	}
+
+	// This method gets the shortest Path from two friends and returns the head
+	// of the linked list
+	public String shortestPath(String start, String end) {
+		ArrayList<Integer> counters = new ArrayList<Integer>();
+		ArrayList<Boolean> visited = new ArrayList<Boolean>();
+		ArrayList<String> shortestPaths = new ArrayList<String>();
+		Queue<String> q = new Queue<String>();
+		boolean flag = false;
+
+		int x = index.get(start);
+		visited.set(x, true);
+		q.enqueue(start);
+		while (!q.isEmpty()) {
+			String w = q.dequeue();
+			int j = index.get(w);
+			Neighbor ptr = adjLL[j].neighbors;
+			while (ptr != null) {
+				if (!visited.get(x)) {
+
+					int temp = counters.get(x);
+					temp = temp++;
+					counters.set(x, temp);
+					int currIndex = index.get(ptr); // look over
+					visited.set(currIndex, true);
+					if (!(ptr.name.equals(end))) {
+						ptr.visitedNames = w;
+						if (flag == false) {
+							flag = true;
+							shortestPaths.set(currIndex, ptr.visitedNames);
+						} else {
+							shortestPaths.set(currIndex,
+									"--".concat(ptr.visitedNames));
+						}
+					}
+					String currName = ptr.name;
+					q.enqueue(currName);
+					ptr = ptr.next;
+				}
+			}
+
+		}
+		int smallestPathIndex = 0;
+		for (int z = 0; z < counters.size(); z++) {
+			int smallestNum = counters.get(z);
+			if (counters.get(z) < smallestNum) {
+				smallestPathIndex = z;
+				smallestNum = counters.get(z);
+
+			}
+		}
+
+		String shortestPath = shortestPaths.get(smallestPathIndex);
+
+		return shortestPath;
+		/*
+		 * we have to add: if found, break adding the shortest path to a data
+		 * structure
+		 */
+	}
+
+	// Gets the cliques from the original graph
+	public Vertex[] cliques(String school, boolean[] visited) {
+		Vertex[] temp = subgraph(school);
+		Vertex[] result = new Vertex[100]; // change this later
+		int v = 0;
+		int counter = 0;
+		dfs(v, visited, temp, result, school, counter);
+		return result;
+
+	}
+
+	// ask if helper methods can return stuff and be public
+	private void dfs(int v, boolean[] visited, Vertex[] subgraph,
+			Vertex[] result, String school, int counter) {
+		visited[v] = true;
+		for (Neighbor e = adjLL[v].neighbors; e != null; e = e.next) {
+			if (!visited[index.get(e.name)]) {
+				// name, inschool, schoolname, neighbor
+				counter++;
+				result[v] = new Vertex(subgraph[v].name, true, school, e);
+				dfs(index.get(e.name), visited, subgraph, result, school,
+						counter);
+			}
+		}
+
+	}
+
+	// This method checks to see if a person is a "connecot" and then it returns
+	// the name of that person.
+	public String connectors() {
+		return null;
+
+	}
+
+	private void printBuild() {
+		Vertex[] temp = adjLL;
+		for (int i = 0; i < temp.length; i++) {
+			Neighbor ptr = temp[i].neighbors;
+			while (ptr != null) {
+				System.out.println(temp[i] + "|" + ptr.name);
+			}
+		}
 	}
 }
