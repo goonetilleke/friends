dc07fa8c822af83892fbbdfcc4d94c2994323b5f
rc678 <rc678@eden.rutgers.edu>
2013-05-03 14:52:04 -0300

    Update Neighbor.java

--- structures/Neighbor.java
+++ structures/Neighbor.java
@@ -2,12 +2,20 @@ package Friend;
 
 public class Neighbor {
 	public String name;
-	public String school;
-	public String inSchool; 
+	//public String school;
+	//public String inSchool; 
 	public Neighbor next;
+	String visitedNames;
+	int DFSNum;
+	int back;
+	String path; 
 
-	public Neighbor(String name, Neighbor nbr) {
+	public Neighbor(String name, Neighbor nbr) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
 		this.name = name;
-		next = nbr;
+		this.next = nbr;
+		this.visitedNames=null;
+		this.DFSNum=0;
+		this.back=0;
+		this.path = ""; 
 	}
 }
