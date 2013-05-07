03bcedf88b9f70141039d0b3e734ab6330891e0c
amandagoonetilleke <ohgoonetilleke@gmail.com>
2013-05-06 22:02:41 -0300

    Update driver.java

--- apps/driver.java
+++ apps/driver.java
@@ -1,9 +1,10 @@
+//Amanda Goonetilleke && Rosheen Chaudhry
 package Friend;
 
 import java.io.*;
 import java.util.Scanner;
 
-public class driver {
+public class Friends {
 
 	static BufferedReader br;
 	private static Scanner sc;
@@ -20,7 +21,7 @@ public class driver {
 		 * vertex method should turn the string into a node and then input the
 		 * node into the adjLL
 		 */
-		Friends friend = null;
+		FriendsGraph friend = null;
 		boolean flag = false;
 		int counter = 0;
 		while (sc.hasNextLine()) {
@@ -28,7 +29,7 @@ public class driver {
 				flag = true;
 				String size = sc.nextLine();
 				int val = Integer.parseInt(size);
-				friend = new Friends(val);
+				friend = new FriendsGraph(val);
 			}
 
 			String s = sc.nextLine();
@@ -37,14 +38,24 @@ public class driver {
 			counter++;
 		}
 		//br = new BufferedReader(new InputStreamReader(System.in));
+		boolean flag2=true;
 		int choice = getChoice();
+		
 		while (choice < 5 && choice > 0) {
+			if (flag2==false){
+			choice=getChoice();
+			}
 			switch (choice) {
 			case 1:// subgraph
 				System.out.println("Enter school");
 				Scanner sc1 = new Scanner(System.in);
 				String input = sc1.nextLine();
-				friend.subgraph(input);
+				if (friend.subgraph(input)==null){
+					System.out.println("No students at this school");
+				}else{
+				friend.printSubgraph(friend.subgraph(input));
+				}
+				flag2=false;
 				break;
 			case 2:// shortest path
 				System.out.println("Enter start name");
@@ -52,7 +63,9 @@ public class driver {
 				String input1 = sc2.nextLine();
 				System.out.println("Enter ending name");
 				String input2 = sc2.nextLine();
-				friend.shortestPath(input1, input2);
+				System.out.println(friend.shortestPath(input1, input2));
+				flag2=false;
+
 				break;
 			// check cliques later!!!!!!!!!!!!!!!!!
 			case 3: // cliques
@@ -60,9 +73,13 @@ public class driver {
 				Scanner sc3 = new Scanner(System.in);
 				String input3 = sc3.nextLine(); 
 				friend.cliques(input3);
+				flag2=false;
+
 				break;
 			case 4:// connectors
 				friend.connectors();
+				flag2=false;
+
 				break;
 			}
 		}
@@ -79,7 +96,8 @@ public class driver {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt(); 
 		while (n < 1 || n > 5) {
-			System.out.print("\tYour choice must be between 1 and 5, reenter => ");
+			System.out.println("\tYour choice must be between 1 and 5, reenter => ");
+			n=in.nextInt();
 		}
 		System.out.println();
 		return n;
